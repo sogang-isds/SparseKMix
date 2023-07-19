b33e08ed1d813d634b7eb5b06727163ceafc8447
JHjang223 <jeahooo4128@gmail.com>
2023-07-19 19:07:45 +0900

    Create create_sparse.sh

---
+++ create_sparse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wav_dir=/home/private_data/KoreanSpeech/test-clean # path to KsponSpeech test-clean
+noise_dir=/home/private_data/wham_noise/cv # path to test WHAM noises
+stage=0
+fs=16000
+all_overlap="0 0.2 0.4 0.6 0.8 1"
+
+set -e
+mkdir -p $out_dir
+
+if [[ $stage -le 0 ]]; then
+    for fs in 16000; do
+      for n_speakers in 2 3; do
+        for ovr_ratio in $all_overlap; do
+          echo "Making mixtures for ${n_speakers} speakers and overlap ${ovr_ratio}"
+          python scripts/make_mixtures.py --wav_dir $wav_dir --noise_dir $noise_dir --rate $fs
+          done
+      done
+    done
+fi
